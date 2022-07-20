dafd22bda1b19d5511dc43f60d681eadf46a9a93
davidlealo <davidlealo@gmail.com>
2022-07-19 23:04:37 -0400

    Estoy buscando la respuesta y dejé un par de notas para después

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,22 @@ try{
     }
     getValorUf()
 
+    /* ESTO NO FUNCIONA 
+
+    async function getListaMonedas() {
+        const resMonedas = await fetch("https://mindicador.cl/api")
+        const dataMonedas = await resMonedas.json()
+        const monedas = document.querySelector('.listaMonedas')
+        monedas.innerHTML = dataMonedas.['codigo']
+    }
+    getListaMonedas()
+
+    La respuesta puede estar en: https://youtu.be/2Xm9P_tXtK8 
+    También revisar: https://youtu.be/rKK1q7nFt7M 
+
+    Aunque parece que la respuesta definitiva está en: https://es.stackoverflow.com/questions/231464/mostrar-datos-en-consola 
+    */
+
 
 
 } catch (e) {
