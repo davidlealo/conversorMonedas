7cf0852151f17e452c7a7e37fac5b5bfd323a48d
davidlealo <davidlealo@gmail.com>
2022-07-23 03:36:04 -0400

    Logro hacer funcionar el background del gráfico, pero no las líneas. No aparece ningún error, entonces no estoy seguro si es que el blanco tapa las líneas

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,8 +3,8 @@ let elegirMoneda = document.getElementById('valoresMonedas');
 let moneda = elegirMoneda.value;
 console.log("La moneda es " + moneda)
 
-function nuevaMoneda () {
-    moneda = elegirMoneda.value;    
+function nuevaMoneda() {
+    moneda = elegirMoneda.value;
     return moneda
 }
 
@@ -34,34 +34,60 @@ const consulta = async () => {
             let operacion = clp / data['serie'][0]['valor'];
             respuestaConsulta.innerHTML = `<p>El resultado es: ${operacion.toFixed(2)} de ${data['nombre']}`
         }
-        
-        
+
+
     } catch (error) {
         error.message
     }
 }
 
 //Función del gráfico
-/*
-const grafico = querySelector('#myChart');
-async () => {let myChart = await new Chart(grafico, {
-    type: 'line',
-    data: {
-        labels: [data['serie'][0]['fecha'], data['serie'][1]['fecha'], data['serie'][2]['fecha'], data['serie'][3]['fecha'], data['serie'][4]['fecha'], data['serie'][5]['fecha'], data['serie'][6]['fecha'], data['serie'][7]['fecha'], data['serie'][8]['fecha'], data['serie'][9]['fecha']],
-        dataset: [{
-            label: `<p>Gráfico de últimos 10 días de ${moneda} `,
-            data: [data['serie'][0]['valor'], data['serie'][1]['valor'], data['serie'][2]['valor'], data['serie'][3]['valor'], data['serie'][4]['valor'], data['serie'][5]['valor'], data['serie'][6]['valor'], data['serie'][7]['valor'], data['serie'][8]['valor'], data['serie'][9]['valor']]
-        }],
+async function getAndCreateDataToChart() {
+    const resGrafico = await fetch(`https://mindicador.cl/api/${moneda}`);
+    const dataGrafico = await resGrafico.json();
+
+    //Eje X
+    const ejeX = [dataGrafico['serie'][0]['fecha'], dataGrafico['serie'][1]['fecha'], dataGrafico['serie'][2]['fecha'], dataGrafico['serie'][3]['fecha'], dataGrafico['serie'][4]['fecha'], dataGrafico['serie'][5]['fecha'], dataGrafico['serie'][6]['fecha'], dataGrafico['serie'][7]['fecha'], dataGrafico['serie'][8]['fecha'], dataGrafico['serie'][9]['fecha']];
+
+    //Eje y 
+    const ejeY = [dataGrafico['serie'][0]['valor'], dataGrafico['serie'][1]['valor'], dataGrafico['serie'][2]['valor'], dataGrafico['serie'][3]['valor'], dataGrafico['serie'][4]['valor'], dataGrafico['serie'][5]['valor'], dataGrafico['serie'][6]['valor'], dataGrafico['serie'][7]['valor'], dataGrafico['serie'][8]['valor'], dataGrafico['serie'][9]['valor']];
+
+    //Dataset
+    const dataset = [{
+        label: 'Valor útimos diez días',
+        borderColor: "rgb(255, 99, 132)",
+        borderWidth: 2,
+        data
     }
-})}
-*/
+    ];
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+
+    return { ejeX, dataset };
+
+}
+
+async function renderGrafica() {
+    const data = await getAndCreateDataToChart();
+    const config = {
+        type: "line",
+        data
+    };
+    const myChart = document.getElementById("myChart");
+    myChart.style.backgroundColor = "white";
+    new Chart(myChart, config);
+}
+
 //Evento del botón
 botonBuscar.addEventListener('click', () => {
     nuevaMoneda();
     console.log("La moneda ahora es " + moneda);
+    renderGrafica();
     consulta();
-
-   // renderGrafica();
-
 })
 
